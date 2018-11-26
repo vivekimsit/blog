34542e42f37f1f31a9ba9bac4acf82036d83e536
Vivek Poddar <vivekimsit@gmail.com>
2018-11-26 11:34:32 +0000

    Changelog:
    1. Crop profile icon.
    2. Add social icons.

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
 // Import typefaces
 import 'typeface-montserrat'
 import 'typeface-merriweather'
+import { SocialIcon } from 'react-social-icons'
 
-import profilePic from './profile-pic.jpg'
+import profilePic from './profile-pic.png'
 import { rhythm } from '../utils/typography'
+
+const IconListContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+
+  a {
+    margin-right: 10px;
+    box-shadow: none;
+  }
+`
 
 class Bio extends React.Component {
   render() {
@@ -13,26 +25,31 @@ class Bio extends React.Component {
       <div
         style={{
           display: 'flex',
+          flexDirection: 'column',
           marginBottom: rhythm(2.5),
         }}
       >
-        <img
-          src={profilePic}
-          alt={`Vivek Poddar`}
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-          }}
-        />
-        <p>
-          Written by <strong>Vivek Poddar</strong> who lives and works in
-          Cambridge building useful things.{' '}
-          <a href="https://twitter.com/vivekimsit">
-            You should follow him on Twitter
-          </a>
-        </p>
+        <div style={{ display: 'flex', marginBottom: rhythm(1) }}>
+          <img
+            src={profilePic}
+            alt={`Vivek Poddar`}
+            style={{
+              marginRight: rhythm(1 / 2),
+              marginBottom: 0,
+              width: rhythm(2),
+              height: rhythm(2),
+            }}
+          />
+          <p style={{ marginBottom: 0 }}>
+            Written by <strong>Vivek Poddar</strong> who lives and works in
+            Cambridge building useful things.{' '}
+          </p>
+        </div>
+        <IconListContainer>
+          <SocialIcon url="http://twitter.com/vivekimsit" />
+          <SocialIcon url="http://github.com/vivekimsit" />
+          <SocialIcon url="http://linkedin.com/in/vivekpoddar" />
+        </IconListContainer>
       </div>
     )
   }
