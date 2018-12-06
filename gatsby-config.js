337708569fba95d31d590cb23dacefb73860ea8a
Vivek Poddar <vivekimsit@gmail.com>
2018-12-06 19:58:37 +0000

    Google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-130575295-1`,
       },
     },
     `gatsby-plugin-feed`,
